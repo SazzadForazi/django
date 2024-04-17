19bfe1e6f80d6348132e5d7f23ea344d145aef45
SazzadForazi <sazzad.csebubt@gmail.com>
2024-04-18 01:30:20 +0600

    add url tag

--- project4/project4/urls.py
+++ project4/project4/urls.py
@@ -18,6 +18,7 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('first',include('myapp.urls')),
-    path('home/',views.home)
+    path('first/',include('myapp.urls')),
+    path('home/',views.home),
+    path('', views.home),
 ]
