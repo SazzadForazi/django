20d44b953e3cb1b6304ce9aecd0e7bb855649c07
SazzadForazi <sazzad.csebubt@gmail.com>
2024-04-21 01:22:49 +0600

    add django-crispy-forms

--- project5/myapp/forms.py
+++ project5/myapp/forms.py
@@ -3,4 +3,11 @@ from django import forms
 class contactForm(forms.Form):
     name = forms.CharField(label="Enter your Name:")
     email = forms.EmailField(label = "User Email")
+    age = forms.IntegerField()
+    weight = forms.FloatField()
+    balance = forms.DecimalField()
+    age = forms.CharField()
+    check = forms.BooleanField()
+    birthday = forms.CharField()
+    appointment = forms.CharField()
     
