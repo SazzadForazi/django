e3418d91445027fda4987099fb51d5c6dd20fb1b
SazzadForazi <sazzad.csebubt@gmail.com>
2024-04-20 17:32:19 +0600

    add api form

--- project5/myapp/urls.py
+++ project5/myapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.home, name="home"),
     path('about/',views.about,name="about"),
-    path('form/',views.submit_form, name="submit_form")
+    path('form/',views.submit_form, name="submit_form"),
+    path('django_forms/',views.Django_forms, name='django_forms' )
 ]
