2e13fea741f6de3f5866b2983262ee0b86972d9f
SazzadForazi <sazzad.csebubt@gmail.com>
2024-04-20 12:41:48 +0600

    updating form

--- project5/myapp/views.py
+++ project5/myapp/views.py
@@ -5,6 +5,12 @@ def home(request):
     return render(request,'home.html')
 
 def about(request):
+       # print(request.POST)
+    if request.method =='POST':
+        name = request.POST.get('username')
+        email = request.POST.get('email')
+        select = request.POST.get('select')
+        return render(request,'about.html',{'name':name, 'email':email, 'select':select})
     return render(request,'about.html')
 
 def submit_form(request):
