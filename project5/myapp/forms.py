57399070fc1784988a9d0d9a5ec9a3d3d9ec73f2
SazzadForazi <sazzad.csebubt@gmail.com>
2024-04-22 06:27:41 +0600

    add attributes & widgets

--- project5/myapp/forms.py
+++ project5/myapp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class contactForm(forms.Form):
-    name = forms.CharField(label="Enter your Name:")
+    # name = forms.CharField(label="Enter your Name:")
     # email = forms.EmailField(label = "User Email")
     # age = forms.IntegerField()
     # weight = forms.FloatField()
@@ -19,3 +19,16 @@ class contactForm(forms.Form):
     file = forms.FileField()
     
     
+    
+    # Attributes ও Widgets
+    
+    name = forms.CharField(label="Full Name : ", help_text="Total length must be within 70 characters", required=False, error_messages={'required': 'Please enter your name.'},widget = forms.Textarea(attrs = {'id' : 'text_area', 'class' : 'class1 class 2', 'placeholder' : 'Enter your name'},))
+    email = forms.EmailField(label = "User Email")
+    age = forms.CharField(widget=forms.NumberInput)
+    check = forms.BooleanField()
+    birthday = forms.CharField(widget=forms.DateInput(attrs= {'type' : 'date'}))
+    appointment = forms.CharField(widget=forms.DateInput(attrs= {'type' : 'datetime-local'}))
+    CHOICES = [('S', 'Small'), ('M', 'Medium'), ('L', 'Large')]
+    size = forms.ChoiceField(choices=CHOICES, widget = forms.RadioSelect)
+    MEAL = [('P', 'Pepperoni'), ('M', 'Mashroom'), ('B', 'Beef')]
+    pizza = forms.MultipleChoiceField(choices=MEAL, widget=forms.CheckboxSelectMultiple)
