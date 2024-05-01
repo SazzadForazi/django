6d5542f76230162357db0a47ddaad2efa14870cf
SazzadForazi <sazzad.csebubt@gmail.com>
2024-05-01 20:13:55 +0600

    add post

--- blog_project/posts/views.py
+++ blog_project/posts/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . import forms
+from . import models
 # Create your views here.
+
 def add_posts(request):
-    return render(request,'add_posts.html')
+    if request.method == 'POST': # user post request koreche
+        post_form = forms.PostForm(request.POST)
+        if post_form.is_valid():
+            post_form.save()
+            return redirect('add_posts')
+    
+    else: # user normally website e gele blank form pabe
+        post_form = forms.PostForm()
+    return render(request, 'add_posts.html', {'form' : post_form})
