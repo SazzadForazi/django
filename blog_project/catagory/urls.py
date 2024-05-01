1ddaf8460aceee83d8016a755352539bf9e4fb8f
SazzadForazi <sazzad.csebubt@gmail.com>
2024-05-01 19:46:55 +0600

    add catagory

--- blog_project/catagory/urls.py
+++ blog_project/catagory/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
 from . import views
 urlpatterns = [
-    path('', views.add_catagory,name='add_catagory' ),
+    path('', views.add_catagory,name='add_catagories' ),
 ]
--- blog_project/catagory/views.py
+++ blog_project/catagory/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . import forms
 # Create your views here.
+
 def add_catagory(request):
-    return render(request,'add_catagory.html')
+    if request.method == 'POST': # if user click submit button
+        category_form = forms.CategoryForm(request.POST) 
+        if category_form.is_valid():
+            category_form.save()
+            return redirect('add_catagories') 
+    
+    else: # user normally website e gele blank form pabe
+        category_form = forms.CategoryForm()
+    return render(request, 'add_catagory.html', {'form' : category_form})
