b78a05a92cb0ba9f38c6f06f4bea2d8d8f36a681
SazzadForazi <sazzad.csebubt@gmail.com>
2024-05-03 23:36:09 +0600

    first part done

--- blog_project/posts/urls.py
+++ blog_project/posts/urls.py
@@ -2,4 +2,6 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.add_posts,name='add_posts' ),
+    path('edit/<int:id>', views.edit_posts,name='edit_posts' ),
+    path('delete/<int:id>', views.delete_posts,name='delete_posts' ),
 ]
--- blog_project/posts/views.py
+++ blog_project/posts/views.py
@@ -8,8 +8,24 @@ def add_posts(request):
         post_form = forms.PostForm(request.POST)
         if post_form.is_valid():
             post_form.save()
-            return redirect('add_posts')
+            return redirect('homepage')
     
     else: # user normally website e gele blank form pabe
         post_form = forms.PostForm()
     return render(request, 'add_posts.html', {'form' : post_form})
+
+def edit_posts(request,id):
+        post = models.Post.objects.get(pk=id)
+        edit_form = forms.PostForm(instance=post)
+        if request.method == 'POST': # user post request koreche
+            edit_form = forms.PostForm(request.POST,instance=post)
+            if edit_form.is_valid():
+                edit_form.save()
+                return redirect('homepage')
+    
+        return render(request, 'add_posts.html', {'form' : edit_form})
+    
+def delete_posts(request,id):
+    post = models.Post.objects.get(pk=id)
+    post.delete()
+    return redirect('homepage')
